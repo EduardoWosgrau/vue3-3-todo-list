304426ec5f0c26a1a906a92d327be6e62cd0b8f9
Eduardo Wosgrau <eduardo.wosgrau@gmail.com>
2023-09-10 17:35:23 -0300

    updated() removed, set storeTodos on local storage

--- scripts.js
+++ scripts.js
@@ -7,10 +7,17 @@ const TodosMachine = {
         }
     },
     methods: {
+        storeTodos: function(){
+            console.log("ok");
+            window.localStorage.setItem("todos", JSON.stringify(this.todos));
+        },
+
         clearTodos: function(){
             this.todos = [];
             window.localStorage.setItem("todos", JSON.stringify(this.todos));
+            this.storeTodos();
         },
+
         addTodo: function(){
             if (!this.newTodo.text){
                 alert("Fill To Do description!");
@@ -21,16 +28,18 @@ const TodosMachine = {
                 "done": false,
             });
             this.newTodo = {};
-            window.localStorage.setItem("todos", JSON.stringify(this.todos));
+            this.storeTodos();
+        },
+        
+        setDoneTodo: function(todo){
+            todo.done = !todo.done;
+            this.storeTodos();
         },
     },
     created(){
         var localTodos = window.localStorage.getItem("todos");
         if (localTodos) this.todos = JSON.parse(localTodos);
     },
-    updated(){
-        window.localStorage.setItem("todos", JSON.stringify(this.todos));
-    }
 };
 
 Vue.createApp(TodosMachine).mount("#app");
