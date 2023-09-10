f7e4789b0055fe584f37af49eaa30887f2972baf
Eduardo Wosgrau <eduardo.wosgrau@gmail.com>
2023-09-10 02:51:50 -0300

    Todos Machine handler

---
+++ scripts.js
@@ -0,0 +1,38 @@
+
+var todos = [
+    {
+        "text": "Test To Do New",
+        "finished": false,
+    },
+    {
+        "text": "Test To Do Finished",
+        "finished": true,
+    },
+];
+
+const TodosMachine = {
+    data(){
+        return {
+            todos: window.todos,
+            newTodo: {},
+        }
+    },
+    methods: {
+        clearTodos: function(){
+            this.todos = [];
+        },
+        addTodo: function(){
+            if (!this.newTodo.text){
+                alert("Fill To Do description!");
+                return;
+            };
+            this.todos.push({
+                "text": this.newTodo.text,
+                "finished": false,
+            });
+            newTodo = {};
+        },
+    },
+};
+
+Vue.createApp(TodosMachine).mount("#app");
